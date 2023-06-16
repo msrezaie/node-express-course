3c4506e5b52104963f3e2c36bd2efd31895c8dc5
Mohammad Rezaie <79583632+msrezaie@users.noreply.github.com>
2023-06-15 23:29:35 -0400

    answers to week3 - unfinished

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,21 @@
-console.log('Express Tutorial')
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// app.use(express.static("./public"));
+// console.log(__dirname);
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "index.html"));
+// });
+
+app.all("*", (req, res) => {
+  res.status(404).send("404");
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running in PORT: ${PORT}`);
+});
