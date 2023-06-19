237508669211af4b8e0ef859c5d6c3a3f1e25779
Mohammad Rezaie <79583632+msrezaie@users.noreply.github.com>
2023-06-19 14:49:02 -0400

    completed week3 assignment
    all tasks completed except bonus assignment

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,14 +1,55 @@
 const express = require("express");
-const path = require("path");
+const { products } = require("./data");
 
 const app = express();
 
-// app.use(express.static("./public"));
-// console.log(__dirname);
+app.use(express.static("./public"));
 
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "index.html"));
-// });
+app.get("/api/v1/test", (req, res) => {
+  res.json({ message: "It worked!" });
+});
+
+app.get("/api/v1/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/v1/products/:productID", (req, res) => {
+  const prodID = parseInt(req.params.productID);
+  const result = products.find((product) => product.id === prodID);
+  result
+    ? res.json(result)
+    : res.status(404).json({ message: "That product was not found." });
+});
+
+app.get("/api/v1/query", (req, res) => {
+  let { search, limit, price } = req.query;
+  let result = [...products];
+  // example: /api/v1/query?search=al
+  if (search) {
+    // example: /api/v1/query?search=/sec/
+    if (search.startsWith("/")) {
+      // the returned query string is in string format, it is converted to a regex object and trailing slashes are removed
+      search = new RegExp(search.slice(1, -1));
+      result = result.filter((product) => search.test(product.name));
+    } else {
+      result = result.filter((product) => product.name.startsWith(search));
+    }
+  }
+  // example: /api/v1/query?price=gt:10
+  if (price) {
+    price = price.split(":");
+    if (price[0] === "gt") {
+      result = result.filter((product) => product.price > price[1]);
+    } else if (price[0] === "lt") {
+      result = result.filter((product) => product.price < price[1]);
+    }
+  }
+  // example: /api/v1/query?limit=3
+  if (limit) {
+    result = result.slice(0, Number(limit));
+  }
+  res.json(result);
+});
 
 app.all("*", (req, res) => {
   res.status(404).send("404");
